54d7cf5aa2d1d2618ac45e93846ce28804ec5b2b
tanatswanashe-zuze <tanatswanashe.zuze@student.moringaschool.com>
2022-03-06 13:33:13 +0200

    var js test

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 //(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
 //let CC = document.getElementById("date")
 //let YY = document.getElementById ("date")
 //let MM = document.getElementById("date")
 //let dd = document.getElementById("date")
 
 
-alert("hey")
+//let dd = getElementById("day");
+//let year = getElementById("Year")
+//let yy = year
+//let MM = getElementById("Month")
+var today = new date()
+var birthdate = new date(datestring)
+var Year = birthdate.getfullyear
+var cc = number(year.substr(0, 2));
+var YY = number(year.substr(2.2))
+
+let birthdate = get
+
+
+
+
+function calculate() {
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+}
