34a325e060349c8e201e35d0dd646b9d4550eddd
tanatswanashe-zuze <tanatswanashe.zuze@student.moringaschool.com>
2022-03-06 09:31:59 +0200

    link main js  with html

---
+++ main.js
@@ -0,0 +1,8 @@
+//(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+//let CC = document.getElementById("date")
+//let YY = document.getElementById ("date")
+//let MM = document.getElementById("date")
+//let dd = document.getElementById("date")
+
+
+alert("hey")
