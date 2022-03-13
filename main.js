43fabc380d8f3584b44297565a7ee876e3b6842a
tanatswanashe-zuze <tanatswanashe.zuze@student.moringaschool.com>
2022-03-13 23:59:35 +0200

    fixed indetation

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 //(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
-//let CC = document.getElementById("date")
-//let YY = document.getElementById ("date")
-//let MM = document.getElementById("date")
-//let dd = document.getElementById("date")
-
-
-//let dd = getElementById("day");
-//let year = getElementById("Year")
-//let yy = year
-//let MM = getElementById("Month")
-//var today = new date()
-//var birthdate = new date(datestring)
 function myF() {
     var Year = document.getElementById("YEAR").value
     var MM = document.getElementById("MONTH").value
@@ -35,12 +23,9 @@ function myF() {
 
     if (Gender == "Male") {
         var AkanName = "Kwasi"
-    } else { AkanName = "Sbo" }
+    } else {
+        AkanName = "Sbo"
+    }
     var AkanNameM = kwasi
-
-
-
-
-
     document.getElementById("test").innerHTML = d
 }
